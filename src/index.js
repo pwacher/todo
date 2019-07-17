11983c3ee8b7b13a0459e925b17a8862e72525e4
pwacher <pwacher@apple.com>
2019-07-17 16:17:29 -0700

    new stuff

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class Header extends React.Component {
+    render () {
+        return (
+            <h1>My Todo App</h1>
+        )
+    }
+}
+
+class NewTodo extends React.Component {
+    input = React.createRef();
+    // TODO: Find out how to do this using a normal function, rather than arrow function
+    addTodo = event => {
+        event.preventDefault();
+        console.log(this.input.current.value);
+        let newOne = this.input.current.value;
+    };
+
+    render () {
+        return (
+            <div>
+                <form onSubmit={this.addTodo}>
+                    <input type="text" ref={this.input} />
+                    <button type="submit">Add a ToDo</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    render () {
+        return (
+            <div>
+                <Header />
+                <NewTodo />
+            </div>
+        )
+    }
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
