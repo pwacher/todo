830508d4524668eff1b3e7c5922c018230e6cae9
pwacher <pwacher@apple.com>
2019-08-20 09:32:57 -0700

    removed button

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const List = props => (
     <ul className="todolist">
         {
-            props.items.map((newItem, index) => <li key={index}>{newItem} <button>Delete</button></li>)
+            props.items.map((newItem, index) => <li key={index}>{newItem}</li>)
         }
     </ul>
 );
